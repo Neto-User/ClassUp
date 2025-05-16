6b9fdf9e6d8fd22bcc4ad2e381d564ff9d2e0f65
Thiago Moreira <thiht93@gmail.com>
2025-05-16 14:27:48 -0300

    Add files via upload

--- ClassUp/src/classup/ClassUp.java
+++ ClassUp/src/classup/ClassUp.java
@@ -1,20 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package classup;
+public class Turma {
+    private String nome;
+    private String disciplina;
+    private String professor;
+    private ColecaoAlunos colecaoAlunos;
+    
 
-/**
- *
- * @author adelv
- */
-public class ClassUp {
+    public Turma(String nome, String disciplina, String professor) {
+        this.nome = nome;
+        this.disciplina = disciplina;
+        this.professor = professor;
+        this.colecaoAlunos = new ColecaoAlunos();
+    }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public String getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(String professor) {
+        this.professor = professor;
+    }
+
+    public void adicionarAluno(Aluno aluno) {
+        this.colecaoAlunos.adicionarAluno(aluno);
     }
     
+    public void removerAluno(Aluno aluno) {
+        this.colecaoAlunos.removerAluno(aluno);
+    }
+
+    public ColecaoAlunos getAlunos() {
+        return this.colecaoAlunos;
+    }
 }
